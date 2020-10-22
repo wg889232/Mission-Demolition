6591d3678ae10c1aa4ad8223e279f5e81ee13460
Wyatt Greenman <59028726+wg889232@users.noreply.github.com>
2020-10-21 23:46:15 -0400

    Added Wooden Walls and Slabs
    Added wooden walls and slabs that are destroyed when impacted by the projectile. They also have half the mass of the regular walls and slabs. Also edited the castles to incorporate the new materials.

---
+++ Assets/__Scripts/WoodWall.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodWall : MonoBehaviour
+{
+    public GameObject woodWall;
+    void OnCollisionEnter(Collision coll)
+    {
+        GameObject collideWith = coll.gameObject;
+        if (collideWith.tag == "Projectile") Destroy(woodWall);
+    }
+}
